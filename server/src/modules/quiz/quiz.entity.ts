be4cb128923db912a1321e70e56d72e9f2d6bb90
Gek <gakidogeck@gmail.com>
2024-05-22 20:07:32 -0700

    trying to setup postgres

---
+++ server/src/modules/quiz/quiz.entity.ts
@@ -0,0 +1,19 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('quizes')
+export class Quiz extends BaseEntity {
+    @PrimaryGeneratedColumn({
+        comment: 'The quiz id'
+    })
+    id: number;
+
+    @Column({
+        type: 'varchar',
+    })
+    title: string;
+
+    @Column({
+        type: 'text',
+    })
+    description: string;
+}
